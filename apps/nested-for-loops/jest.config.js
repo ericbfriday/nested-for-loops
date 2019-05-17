d2a0fb89a3113a59ac3a5e601e978b0f7c25d667
Friday, Eric <bxn4@ad.moneygram.com>
2019-05-17 16:13:58 -0500

    initial commit

---
+++ apps/nested-for-loops/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "nested-for-loops",
+  preset: "../../jest.config.js",
+  coverageDirectory: "../../coverage/apps/nested-for-loops/",
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
+};
---
+++ apps/nested-for-loops/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "friday-friday-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = "nested-for-loops";
+}
